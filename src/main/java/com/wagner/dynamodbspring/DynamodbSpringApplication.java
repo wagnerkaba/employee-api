0f665fb98c8e924557bc82a2d6ce14280553b658
wagnerkaba <bom.link+github@gmail.com>
2021-11-29 16:09:35 -0300

    First commit

---
+++ src/main/java/com/wagner/dynamodbspring/DynamodbSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wagner.dynamodbspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamodbSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagner/dynamodbspring/DynamodbSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagner.dynamodbspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
